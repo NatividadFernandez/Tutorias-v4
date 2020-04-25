c342245bc01b5a945f780b2926cfa24e81414966
unknown <nati.fernandez.alcala@gmail.com>
2020-04-25 12:36:54 +0200

    first commit

---
+++ src/main/java/org/iesalandalus/programacion/tutorias/MainApp.java
@@ -0,0 +1,20 @@
+package org.iesalandalus.programacion.tutorias;
+
+import org.iesalandalus.programacion.tutorias.mvc.controlador.Controlador;
+import org.iesalandalus.programacion.tutorias.mvc.controlador.IControlador;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.FactoriaFuenteDatos;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.IModelo;
+import org.iesalandalus.programacion.tutorias.mvc.modelo.Modelo;
+import org.iesalandalus.programacion.tutorias.mvc.vista.FactoriaVista;
+import org.iesalandalus.programacion.tutorias.mvc.vista.IVista;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		Modelo modelo = new Modelo(FactoriaFuenteDatos.FICHEROS.crear());
+		IVista vista = FactoriaVista.TEXTO.crear();
+		IControlador controlador = new Controlador(modelo, vista);
+		controlador.comenzar();
+	}
+
+}
